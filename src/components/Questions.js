50bc7c734ec8f7080f3cc86b9e074496fb4de4df
Manuel C Solis <manuelsolis@Manuels-Air.localdomain>
2018-09-25 13:33:01 -0400

    Added Question props, not yet tested. Ready to create JSON file

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -5,7 +5,8 @@ class Questions extends Component {
 constructor(props) {
     super(props);
     this.state = {
-        currentQuestion:1
+        currentQuestion:1,
+        currentAnswer:1
     }
 }
 
@@ -21,10 +22,25 @@ addOneToQuestion = () => {
     
 }
 
+updateCurrentAnswer = (nextAnswer) => {
+    this.setState({
+        currentAnswer: nextAnswer
+    })
+}
+
+addOneToAnswer = () => {
+    if (this.state.currentAnswer < 10)
+    this.updateCurrentAnswer(this.state.currentAnswer + 1)
+    
+}
 
     render() {
         return (
         <div className="App">
+        <section className="ActualQuestion">
+        <h3>What do you like as symbol?</h3>
+        </section>    
+
         <section className="img-fluid">
             <img onClick={this.addOneToQuestion} className="image" src={`./images/Question-${this.state.currentQuestion}/Picture1Question${this.state.currentQuestion}.png`} alt="P1Q1" height="300" width="300"/>
             <img onClick={this.addOneToQuestion} className="image" src={`./images/Question-${this.state.currentQuestion}/Picture2Question${this.state.currentQuestion}.png`} alt="P1Q1" height="300" width="300"/>
