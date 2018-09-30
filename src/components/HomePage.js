7db8ad7ee2ed5b665c7f3f6704dbedd663d56d3b
Manuel C Solis <manuelsolis@Manuels-MacBook-Air.local>
2018-09-30 10:20:02 -0400

    Updated json, questions link needs to be fixed

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
+import Questions from './Questions';
+import logo from './logo.svg';
 
 class HomePage extends Component {
     render() {
         return (
             <div>
                 <section className="container-fluid">
-                  <img className="test-image" src="./images/Who-Am-I.png"
+                  <img className="test-image" src="./images/my-personality-test.jpg"
                     alt="logo"/>
                   
                   <section className="App-intro">Who Am I?</section>
                   <section>AN IN-DEPTH PERSONALITY TEST DESIGNED BY PSYCOLOGISTS</section>
                   <br></br>
-                  <button type="button" className="btn btn-primary btn-lg">Start Quiz!</button>
-                  {/* <button className="Start-Quiz-Button">Start Quiz!</button> */}
+                  <button type="button" className="btn btn-primary btn-lg">{this.state.currentQuestion}</button>
                   </section>
                   <br/>
                   <br/>
-                <section className="footer">
-                <footer>
-                    Powered by 
-                </footer>
+                <section>
+                    <footer className="footer">
+                        Powered by <img src={logo} className="App-logo" alt="logo"/>
+                    </footer>
                 </section>
             </div>
         );
