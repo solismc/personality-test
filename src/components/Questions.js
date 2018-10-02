257b503128c11a8e55218b803adfd8aec266e989
Manuel C Solis <manuelsolis@Manuels-MacBook-Air.local>
2018-10-02 13:49:17 -0400

    Keeps score. Need to plug data from json to questions and results

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -7,32 +7,26 @@ constructor(props) {
     super(props);
     this.state = {
         currentQuestion:1,
-        currentAnswer:1
+        currentAnswer:1,
+        score:0
     }
 }
 
-updateCurrentQuestion = (nextQuestion) => {
+updateCurrentQuestion = (nextQuestion, questionValue) => {
     this.setState({
-        currentQuestion: nextQuestion
+        currentQuestion: nextQuestion,
+        score: this.state.score + questionValue
     })
 }
 
-addOneToQuestion = () => {
-    if (this.state.currentQuestion < 10)
-    this.updateCurrentQuestion(this.state.currentQuestion + 1)
-    
+addOneToQuestion = (questionValue) => {
+    if (this.state.currentQuestion <= 10){
+        this.updateCurrentQuestion(this.state.currentQuestion + 1, questionValue)
+    } else {
+        // redirect here 
+    }
 }
 
-updateCurrentAnswer = (nextAnswer) => {
-    this.setState({
-        currentAnswer: nextAnswer
-    })
-}
-
-addOneToAnswer = () => {
-    if (this.state.currentAnswer < 10)
-    this.updateCurrentAnswer(this.state.currentAnswer + 1)
-}
 
     render() {
         return (
@@ -42,10 +36,10 @@ addOneToAnswer = () => {
         </section>    
 
         <section className="img-fluid">
-            <img onClick={this.addOneToQuestion} className="image" src={`./images/Question-${this.state.currentQuestion}/Picture1Question${this.state.currentQuestion}.png`} alt="P1Q1" height="300" width="300"/>
-            <img onClick={this.addOneToQuestion} className="image" src={`./images/Question-${this.state.currentQuestion}/Picture2Question${this.state.currentQuestion}.png`} alt="P1Q1" height="300" width="300"/>
-            <img onClick={this.addOneToQuestion} className="image" src={`./images/Question-${this.state.currentQuestion}/Picture3Question${this.state.currentQuestion}.png`} alt="P1Q1" height="300" width="300"/>
-            <img onClick={this.addOneToQuestion} className="image" src={`./images/Question-${this.state.currentQuestion}/Picture4Question${this.state.currentQuestion}.png`} alt="P1Q1" height="300" width="300"/>
+            <img onClick={() => this.addOneToQuestion(10)} className="image" src={`./images/Question-${this.state.currentQuestion}/Picture1Question${this.state.currentQuestion}.png`} alt="P1Q1" height="300" width="300"/>
+            <img onClick={() =>this.addOneToQuestion(20)} className="image" src={`./images/Question-${this.state.currentQuestion}/Picture2Question${this.state.currentQuestion}.png`} alt="P1Q1" height="300" width="300"/>
+            <img onClick={() =>this.addOneToQuestion(30)} className="image" src={`./images/Question-${this.state.currentQuestion}/Picture3Question${this.state.currentQuestion}.png`} alt="P1Q1" height="300" width="300"/>
+            <img onClick={() =>this.addOneToQuestion(40)} className="image" src={`./images/Question-${this.state.currentQuestion}/Picture4Question${this.state.currentQuestion}.png`} alt="P1Q1" height="300" width="300"/>
         </section>
         <section>
             <footer className="footer">
