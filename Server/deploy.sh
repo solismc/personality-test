01fc294d6eaae3177ca24bd94fad93eed473fea6
Manuel Solis <manolocsolis@gmail.com>
2019-12-20 14:48:01 -0500

    fixed a build error

--- Server/deploy.sh
+++ Server/deploy.sh
@@ -4,7 +4,7 @@ cp dockerfile ./bin/release/netcoreapp2.1/publish
 
 docker build -t my-personality-test-image ./bin/release/netcoreapp2.1/publish
 
-docker tag my-personality-test-image registry.heroku.com/my-personality-test/web
+docker build -t my-personality-test-image ./bin/release/netcoreapp2.1/publish
 
 docker push registry.heroku.com/my-personality-test/web
 
